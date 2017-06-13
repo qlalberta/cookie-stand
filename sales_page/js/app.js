d646883baec5a82f979022fbc10a9e4576ab6a34
ql24 <qlalberta@gmail.com>
2017-06-13 14:10:24 -0700

    simplied some code; fixed a minr error related to the fonts

--- sales_page/js/app.js
+++ sales_page/js/app.js
@@ -134,21 +134,44 @@ for (var m = 0; m < 15; m++) {
 
 //store the hours in an array
 //store the morning hours
-var hourAm = 5;
-var hourList1 = [];
-for (var j = 6; j < 12; j++) {
-  hourAm++;
-  hourList1.push(hourAm + 'am');
-}
-hourList1.push('12pm');
+// var hourAm = 5;
+// var hourList1 = [];
+// for (var j = 6; j < 12; j++) {
+//   hourAm++;
+//   hourList1.push(hourAm + 'am');
+// }
+// hourList1.push('12pm');
+// //store the after hours into the same array
+// var hourPm = 0;
+// for (var k = 1; k < 9; k++) {
+//   hourPm++;
+//   hourList1.push(hourPm + 'pm');
+//   console.log(hourList1);
+// }
 
-//store the after hours into the same array
-var hourPm = 0;
-for (var k = 1; k < 9; k++) {
-  hourPm++;
-  hourList1.push(hourPm + 'pm');
-  console.log(hourList1);
+
+var hourList = [];
+var hourText = 6;
+var j = 6;
+for (j = 6; j < 21; j++) {
+  if (hourText < 12) {
+    hourList.push(hourText + 'am');
+    hourText++;
+    console.log(hourList);
+  }
+  else if (hourText == 12) {
+    hourList.push(hourText + 'pm');
+    hourText++;
+  }
+  else {
+    hourText -= 12;
+    hourList.push(hourText + 'pm');
+    hourText += 12;
+    hourText++;
+  }
 }
+
+
 
 // generate the list on the browser
 //The list should have
@@ -157,84 +180,84 @@ for (var k = 1; k < 9; k++) {
 // get (from the DOM) who the parent element is going to be. where am I attaching this new element
 
 
-var parentElement1 = document.getElementById('sales');
-
+var parentElement = document.getElementById('sales');
+var article = document.createElement('article');
+parentElement.appendChild(article);
 var h2_1 = document.createElement('h2');
-parentElement1.appendChild(h2_1);
+article.appendChild(h2_1);
 h2_1.textContent = avgCookie[0][0];
 var ul_1 = document.createElement('ul');
-h2_1.appendChild(ul_1);
+article.appendChild(ul_1);
 
 for (var n = 0; n < 16; n++) {
   var li_1 = document.createElement('li');
-  // var positionUl1 = document.getElementByTagName('ul');
   ul_1.appendChild(li_1);
   if (n != 15 ) {
-    li_1.textContent = hourList1[n] + ': ' + pikeSalesPerHourList[n] + ' cookies';
+    li_1.textContent = hourList[n] + ': ' + pikeSalesPerHourList[n] + ' cookies';
   } else {
     li_1.textContent = 'Total: ' + pikeSum + ' cookies';
   }
 }
 
 var h2_2 = document.createElement('h2');
-parentElement1.appendChild(h2_2);
+article.appendChild(h2_2);
 h2_2.textContent = avgCookie[1][0];
 var ul_2 = document.createElement('ul');
-h2_2.appendChild(ul_2);
+article.appendChild(ul_2);
 
 for (var o = 0; o < 16; o++) {
   var li_2 = document.createElement('li');
   ul_2.appendChild(li_2);
   if (o != 15 ) {
-    li_2.textContent = hourList1[o] + ': ' + airportSalesPerHourList[o] + ' cookies';
+    li_2.textContent = hourList[o] + ': ' + airportSalesPerHourList[o] + ' cookies';
   } else {
     li_2.textContent = 'Total: ' + airportSum + ' cookies';
   }
 }
 
 var h2_3 = document.createElement('h2');
-parentElement1.appendChild(h2_3);
+article.appendChild(h2_3);
 h2_3.textContent = avgCookie[2][0];
 var ul_3 = document.createElement('ul');
-h2_3.appendChild(ul_3);
+article.appendChild(ul_3);
 
 for (var p = 0; p < 16; p++) {
   var li_3 = document.createElement('li');
   ul_3.appendChild(li_3);
   if (p != 15 ) {
-    li_3.textContent = hourList1[p] + ': ' + seattleCenterSalesPerHourList[p] + ' cookies';
+    li_3.textContent = hourList[p] + ': ' + seattleCenterSalesPerHourList[p] + ' cookies';
   } else {
     li_3.textContent = 'Total: ' + seattleCenterSum + ' cookies';
   }
 }
 
 var h2_4 = document.createElement('h2');
-parentElement1.appendChild(h2_4);
+article.appendChild(h2_4);
 h2_4.textContent = avgCookie[3][0];
 var ul_4 = document.createElement('ul');
-h2_4.appendChild(ul_4);
+article.appendChild(ul_4);
 
 for (var q = 0; q < 16; q++) {
   var li_4 = document.createElement('li');
   ul_4.appendChild(li_4);
   if (q != 15 ) {
-    li_4.textContent = hourList1[q] + ': ' + capitalHillSalesPerHourList[q] + ' cookies';
+    li_4.textContent = hourList[q] + ': ' + capitalHillSalesPerHourList[q] + ' cookies';
   } else {
     li_4.textContent = 'Total: ' + capitalHillSum + ' cookies';
   }
 }
 
 var h2_5 = document.createElement('h2');
-parentElement1.appendChild(h2_5);
+article.appendChild(h2_5);
 h2_5.textContent = avgCookie[4][0];
 var ul_5 = document.createElement('ul');
-h2_5.appendChild(ul_5);
+article.appendChild(ul_5);
 
 for (var r = 0; r < 16; r++) {
   var li_5 = document.createElement('li');
   ul_5.appendChild(li_5);
   if (r != 15 ) {
-    li_5.textContent = hourList1[r] + ': ' + alkiSalesPerHourList[r] + ' cookies';
+    li_5.textContent = hourList[r] + ': ' + alkiSalesPerHourList[r] + ' cookies';
   } else {
     li_5.textContent = 'Total: ' + alkiSum + ' cookies';
   }
