fdabbd71769c5d3fb0a7144d091aed2914bf13b4
ql24 <qlalberta@gmail.com>
2017-06-24 19:52:15 -0700

    finished revising day8lab

--- sales_page/js/app.js
+++ sales_page/js/app.js
@@ -6,6 +6,7 @@ var avgCookie = [['1st and Pike', 6.3], ['SeaTac Airport', 1.2], ['Seattle Cente
 var minCustomer = [['1st and Pike', 23], ['SeaTac Airport', 3], ['Seattle Center', 11], ['Capitol Hill', 20], ['Alki', 2]];
 var maxCustomer = [['1st and Pike', 65], ['SeaTac Airport', 24], ['Seattle Center', 38], ['Capitol Hill', 38], ['Alki', 16]];
 Location = [avgCookie[0][0], avgCookie[1][0], avgCookie[2][0], avgCookie[3][0]];
+var columnNumber = 16;
 //generate ranndom numbers of customer per hour
 function randomCustomerNumber (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -34,7 +35,7 @@ Sales.prototype.getSalesPerHourList = function () {
 };
 
 //create prototype method render(); specify the function at the bottom
-Sales.prototype.render = render;
+Sales.prototype.renderRows = renderRows;
 
 // create new functions for the five locations
 var pikeSales = new Sales(avgCookie[0][0],avgCookie[0][1],minCustomer[0][1],maxCustomer[0][1]);
@@ -44,61 +45,75 @@ var seattleCenterSales =
 var capitalHillSales = new Sales(avgCookie[3][0],avgCookie[3][1],minCustomer[3][1],maxCustomer[3][1]);
 var alkiSales = new Sales(avgCookie[4][0],avgCookie[4][1],minCustomer[4][1],maxCustomer[4][1]);
 
-//call functions to genernate salesPerHourList
+//store the hour output in an array hourList
+function generateHourList () {
+  var hourList = [''];
+  var hourText = 6;
+  var j = 6;
+  for (j = 6; j < 21; j++) {
+    if (hourText < 12) {
+      hourList.push(hourText + 'am');
+      hourText++;
+    }
+    else if (hourText == 12) {
+      hourList.push(hourText + 'pm');
+      hourText++;
+    }
+    else {
+      hourText -= 12;
+      hourList.push(hourText + 'pm');
+      hourText += 12;
+      hourText++;
+    }
+  }
+  return hourList;
+}
+
+//create a function to generate the total sales per hour at each location
+function generateTotalSales() {
+  var totalSales = [];
+  for(var k = 0; k < 15; k++) {
+    totalSales[k] = pikeSales.salesPerHourList[k] + airportSales.salesPerHourList[k] + seattleCenterSales.salesPerHourList[k] + capitalHillSales.salesPerHourList[k] + alkiSales.salesPerHourList[k];
+  }
+  return totalSales;
+}
+//call functions to genernate salesPerHourLis and hourList
 pikeSales.getSalesPerHourList();
 airportSales.getSalesPerHourList();
 seattleCenterSales.getSalesPerHourList();
 capitalHillSales.getSalesPerHourList();
 alkiSales.getSalesPerHourList();
+var hourList = generateHourList();
+var totalSales = generateTotalSales();
 
-//store the hour output in an array hourList
-var hourList = [''];
-var hourText = 6;
-var j = 6;
-for (j = 6; j < 21; j++) {
-  if (hourText < 12) {
-    hourList.push(hourText + 'am');
-    hourText++;
-  }
-  else if (hourText == 12) {
-    hourList.push(hourText + 'pm');
-    hourText++;
-  }
-  else {
-    hourText -= 12;
-    hourList.push(hourText + 'pm');
-    hourText += 12;
-    hourText++;
-  }
-}
-
-//store the sum of 5 different location sales per hour
-var totalSales = [];
-
-for(var k = 0; k < 15; k++) {
-  totalSales[k] = pikeSales.salesPerHourList[k] + airportSales.salesPerHourList[k] + seattleCenterSales.salesPerHourList[k] + capitalHillSales.salesPerHourList[k] + alkiSales.salesPerHourList[k];
-}
-
-// generate the table on the browser
+// create functions to display the table using calculated data
 //create a table
-var parentElement = document.getElementById('sales');
-var article = document.createElement('article');
-parentElement.appendChild(article);
-var table = document.createElement('table');
-article.appendChild(table);
-
-// create the table header
-var row_0 = document.createElement('tr');
-table.appendChild(row_0);
-for (var l = 0; l < hourList.length; l++) {
-  var th = document.createElement('th');
-  th.textContent = hourList[l];
-  row_0.appendChild(th);
+function createTable() {
+  var parentElement = document.getElementById('sales');
+  var article = document.createElement('article');
+  parentElement.appendChild(article);
+  var table = document.createElement('table');
+  article.appendChild(table);
+  return table;
 }
 
-//other rows for sales from 5 locations.
-function render () {
+//create the table header
+var table = createTable();
+function createTableHeader() {
+  var row_0 = document.createElement('tr');
+  table.appendChild(row_0);
+  for (var l = 0; l < columnNumber; l++) {
+    var th = document.createElement('th');
+    th.textContent = hourList[l];
+    row_0.appendChild(th);
+  }
+  return row_0;
+}
+
+//function to disply other rows for sales from 5 locations.
+function renderRows () {
   var row_1 = document.createElement('tr');
+  table.appendChild(row_1);
   var td = document.createElement('td');
   td.textContent = this.location;
   row_1.appendChild(td);
@@ -107,15 +122,32 @@ function render () {
     td.textContent = this.salesPerHourList[l];
     row_1.appendChild(td);
   }
-  table.appendChild(row_1);
+  return row_1;
 }
 
-//call function render to display the form
-pikeSales.render();
-airportSales.render();
-seattleCenterSales.render();
-capitalHillSales.render();
-alkiSales.render();
+//function to display the total sales
+// var row_1 = renderRows();
+function renderTotalSales() {
+  var row_2 = document.createElement('tr');
+  table.appendChild(row_2);
+  var td = document.createElement('td');
+  td.textContent = 'Totals';
+  row_2.appendChild(td);
+  for (var l = 0; l < 15; l++) {
+    td = document.createElement('td');
+    td.textContent = totalSales[l];
+    row_2.appendChild(td);
+  }
+}
+//call function to display the form
+createTable();
+createTableHeader();
+pikeSales.renderRows();
+airportSales.renderRows();
+seattleCenterSales.renderRows();
+capitalHillSales.renderRows();
+alkiSales.renderRows();
+renderTotalSales();
 
 // generate form from input and append it to the table
 var salesForm = document.getElementById('addSalesForm');
